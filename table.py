b17b0eeb4791ff78ba56ed0ce873ecc5aab1b50f
abhijit3009 <50164918+abhijit3009@users.noreply.github.com>
2020-01-14 13:23:16 +0200

    Update table.py

--- table.py
+++ table.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-connection = sqlite3.connect('data.db')#creating data.db database using connection(connect)
-cursor = connection.cursor()# creatting a Cursor object and call its execute() method to perform SQL commands
+connection = sqlite3.connect('data.db')#creating data.db database 
+cursor = connection.cursor()# creatting a Cursor object 
 
 #creating a TABLE for users
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
@@ -9,8 +9,8 @@ cursor.execute(create_table)
 #creating a TABLE for items
 create_table = "CREATE TABLE IF NOT EXISTS items (name text PRIMARY KEY, price real)"
 cursor.execute(create_table)
-#inserting data for testitem
-cursor.execute("INSERT INTO items VALUES ('testitem', '123456789')")
+#inserting data for test item
+cursor.execute("INSERT INTO items VALUES ('test item', '123456789')")
 
-connection.commit() # Save (commit) the changes and close
+connection.commit() 
 connection.close()
