e6d9329910fafb02817e0509b33e0ff5e87b134e
abhijit3009 <50164918+abhijit3009@users.noreply.github.com>
2020-01-14 12:50:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-# Flask class is imported from flask module
-from flask import Flask, request
-# abort, Resource, Api classes imported from flask_restful module inorder to create the first rest api
-from flask_restful import Resource, Api, abort, reqparse
+from flask import Flask, request # Flask is imported from module
 
-#import JWT,jwt_required, current_identity from flask_jwt module which is used for authentication
-from flask_jwt import JWT, jwt_required, current_identity
+from flask_restful import Resource, Api, abort, reqparse # all file classes are imported from flask_restful
+
+
+from flask_jwt import JWT, jwt_required, current_identity#import JWT,jwt_required, current_identity from flask_jwt module
 
 #import authenticate and identity from securty.py previously created.
 from security import authenticate, identity
 from user import UserRegister
 from item import Item
 
-# Flask application instance is created using below command
+# Flask application is created
 app = Flask(__name__)
 
 # Adding a secreate line
-app.secret_key = 'FirstJson1'
+app.secret_key = 'abhi'
 
-#Api application instance is created using below command
+#Api application instance is created 
 api = Api(app)
 
-#create a JWT object. As soon as this JWT object created, Flask-JWT registers an endpoint with our application, /auth
+#create a JWT object. 
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/<name>')
