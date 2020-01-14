2e405524a6cdfc532582c446ed892e17e6af84ce
abhijit3009 <50164918+abhijit3009@users.noreply.github.com>
2020-01-14 12:59:39 +0200

    Update item.py

--- item.py
+++ item.py
@@ -3,24 +3,23 @@ from flask_jwt import JWT, jwt_required, current_identity
 
 import sqlite3
 
-# generally API works with resources and resoruces should be defined as class. item and itemList is created as Resource class
+# item and itemList is created as Resource class
 # Resource item definition
-# for GET and DELETE method, verifying if the  value exists. if doesnt exists, report error code 404 and print itemX doesnt exist message line 14
-# Inside any endpoint that is decorated with @jwt_required(), can access the current_identity proxy—
-# it will give us whatever the identity function returns for the JWT we received in this specific request.
+# for GET and DELETE method, verifying if the  value exists.
+
 
 class Item(Resource):
     TABLE_NAME = 'items'
     parser = reqparse.RequestParser()
     parser.add_argument('price', type=float,required=True, help='This field cannot be left blank')
-    # GET method to print an specific item
+    # GET method to print an item
     @jwt_required()
     def get(self, name):
         item = self.find_by_name(name)
         if item:
             return item
         return {'message':'Item not found'}, 404
-    # POST method to print an specific item
+    # POST method to print an item
     @jwt_required()
     def post(self, name):
         if self.find_by_name(name):
@@ -32,7 +31,7 @@ class Item(Resource):
         except:
             return {"message":"An error occured during inserting item"}, 500
         return item, 201
-        # PUT method to print an specific item
+        # PUT method to print an item
     @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
@@ -51,7 +50,7 @@ class Item(Resource):
                 return {"message":"an error occured during update an item"}
         return update_item
 
-    # finding the item in the list
+    
     @classmethod
     def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
@@ -63,7 +62,7 @@ class Item(Resource):
         if row:
             return {'item': {'name':row[0], 'price':row[1]}} #return the item if it exists
 
-    # definition of insert for POST and PUT methods
+    # for POST and PUT methods
     @classmethod
     def insert(cls, item):
         #connection to database
@@ -76,7 +75,7 @@ class Item(Resource):
         connection.close()
         return {'message':'Item Added'}   
 
-    #update method for PUT methods
+    # PUT methods
     @classmethod
     def update(cls, item):
         connection = sqlite3.connect('data.db')
