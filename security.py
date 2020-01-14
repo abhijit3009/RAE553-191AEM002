764fed3a819bafad05a2c0326287ed2cd1706959
abhijit3009 <50164918+abhijit3009@users.noreply.github.com>
2020-01-14 13:05:19 +0200

    Update security.py

--- security.py
+++ security.py
@@ -1,13 +1,13 @@
-# importing safe_str_cmp from werkzeug to compare strings
+# importing safe_str_cmp from werkzeug
 from werkzeug.security import safe_str_cmp
 from user import User
 
-# authenticate
+# authentication
 def authenticate(username, password):
     user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
-#identify
+#identification
 def identity(payload):
     user_id = payload['identity']
     return User.find_by_id(user_id)
